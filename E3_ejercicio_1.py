cf8719449b20358d3dcaa9e0fda89e2a92dd90df
SebastianOMDuoc <se.olivaresm@duocuc.cl>
2025-06-17 20:13:59 -0400

    Se añadió un input para indicar el número de dosis para un esquema completo.

--- E3_ejercicio_1.py
+++ E3_ejercicio_1.py
@@ -4,19 +4,20 @@ contador_esquema_incompleto=0
 while True:
     try:
         registro_personas=int(input("Ingrese la cantidad de personas a registrar: "))
+        esquema_completo=int(input("Ingrese la cantidad vacunas para completar el esquema: "))
         break
     except:
-        print("Error en el ingreso de registro. Por favor intente con un número entero")
+        print("Error en el ingreso de registro o ingreso de dosis en el esquema. Por favor intente con un número entero")
     
 for i in range(registro_personas):
     while True:
         try:
             condicion_esquema=int(input("Ingrese cantidad de dosis recibidas: "))
-            if condicion_esquema < 3:
+            if condicion_esquema < esquema_completo:
                 print("Esquema incompleto")
                 contador_esquema_incompleto += 1
                 break
-            elif condicion_esquema == 3:
+            elif condicion_esquema == esquema_completo:
                 print("Esquema completo")
                 contador_esquema_completo += 1
                 break
